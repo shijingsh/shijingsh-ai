b90b3646e8eeee9dfbed97dc11a20a7f1672aa0a
liukefu2050 <liukefu2050@sina.com>
2020-03-01 13:23:38 +0800

    首个没有编译错误版本

---
+++ shijingsh-ai-core/src/main/java/com/shijingsh/core/resource/schema/ResourceStorageFactory.java
@@ -0,0 +1,79 @@
+package com.shijingsh.core.resource.schema;
+
+import java.util.Collection;
+import java.util.Map;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+import com.shijingsh.core.resource.ResourceStorage;
+import com.shijingsh.core.resource.format.FormatAdapter;
+import com.shijingsh.core.resource.path.PathAdapter;
+
+/**
+ * 仓储管理器工厂
+ *
+ * @author Birdy
+ */
+public class ResourceStorageFactory extends DefaultListableBeanFactory implements ApplicationContextAware, FactoryBean<ResourceStorage> {
+
+    public static final String DEFINITIONS = "definitions";
+
+    private ApplicationContext applicationContext;
+
+    /** 仓储定义列表 */
+    private Collection<Class<?>> definitions;
+
+    private FormatAdapter format;
+
+    private PathAdapter path;
+
+    private ResourceStorage storage;
+
+    public void setDefinitions(Collection<Class<?>> definitions) {
+        this.definitions = definitions;
+    }
+
+    public void setFormat(FormatAdapter format) {
+        this.format = format;
+    }
+
+    public void setPath(PathAdapter path) {
+        this.path = path;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+        // TODO 将applicationContext设置为StorageManagerFactory的工厂.
+        this.setParentBeanFactory(this.applicationContext);
+        // 支持属性文件(*.properties)
+        Map<String, PropertyPlaceholderConfigurer> propertyConfigurers = this.applicationContext.getBeansOfType(PropertyPlaceholderConfigurer.class);
+        for (PropertyPlaceholderConfigurer ropertyConfigurer : propertyConfigurers.values()) {
+            ropertyConfigurer.postProcessBeanFactory(this);
+        }
+    }
+
+    @Override
+    public synchronized ResourceStorage getObject() throws Exception {
+        if (storage == null) {
+            storage = ResourceStorage.instanceOf(definitions, format, path);
+        }
+        return storage;
+    }
+
+    @Override
+    public Class<ResourceStorage> getObjectType() {
+        return ResourceStorage.class;
+    }
+
+    @Override
+    public boolean isSingleton() {
+        return true;
+    }
+
+}
---
+++ shijingsh-ai-core/src/main/java/com/shijingsh/core/resource/schema/ResourceXmlHandler.java
@@ -0,0 +1,19 @@
+package com.shijingsh.core.resource.schema;
+
+import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
+
+import com.shijingsh.core.resource.schema.ResourceXmlParser.ElementDefinition;
+
+/**
+ * 仓储XML处理器
+ *
+ * @author Birdy
+ */
+public class ResourceXmlHandler extends NamespaceHandlerSupport {
+
+    @Override
+    public void init() {
+        registerBeanDefinitionParser(ElementDefinition.CONFIGURATION.getName(), new ResourceXmlParser());
+    }
+
+}
